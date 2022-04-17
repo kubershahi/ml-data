08fcbe225557dab3a0bb4378af04c2af68f15680
Kuber Shahi <shahikuber97@gmail.com>
2022-04-17 23:11:09 +0530

    repo clean deleted blockchain project

--- bc/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-const Greeter = artifacts.require("Greeter");
-const Operation = artifacts.require("Operation");
-
-module.exports = function (deployer) {
-  deployer.deploy(Greeter);
-  deployer.deploy(Operation);
-};
